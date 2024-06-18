1573e6ae652e6f58287d4e5180ef9b02964f1c84
taisei1103 <suwonler@gmail.com>
2024-06-19 00:50:01 +0900

    Update minsub.py

--- pages/minsub.py
+++ pages/minsub.py
@@ -8,21 +8,27 @@ st.write('# 1. 수시')
 
 import numpy as np   # numpy 라이브러리 임포트
 import pandas as pd
-
-# 데이터 설정
 data = {
     '전형 유형': ['학생부교과', '학생부종합', '농어촌전형', '학교생활우수자'],
     '평균 등급': [1.85, 2.49, 1.78, 2.49]
 }
-
-# 데이터프레임 생성
 df = pd.DataFrame(data)
-
-# 전형 유형을 인덱스로 설정
 df.set_index('전형 유형', inplace=True)
+st.title('수시 입결')
+st.bar_chart(df)
 
-# Streamlit 제목 설정
-st.title('전자전기공학부 2023학년도 입시 결과')
+st.write('# 2. 정시')
+import streamlit as st
+import pandas as pd
+import numpy as np
 
-# 바 차트 생성 및 표시
+# 데이터 설정
+subjects = np.array(['국어', '영어', '수학', '탐구', '평균'])
+scores = np.array([85, 1.91, 92, 84.5, 87.17])
+df = pd.DataFrame({
+    '과목': subjects,
+    '점수': scores
+})
+df.set_index('과목', inplace=True)
+st.title('정시 입결')
 st.bar_chart(df)
