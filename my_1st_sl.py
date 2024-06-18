1313a9a3cc9be6b788a77b6b413886c16afce2a6
taisei1103 <suwonler@gmail.com>
2024-06-19 00:09:04 +0900

    Update my_1st_sl.py

--- my_1st_sl.py
+++ my_1st_sl.py
@@ -4,13 +4,22 @@ import streamlit as st  # streamlit 라이브러리 임포트
 # 타이틀 텍스트 출력
 st.title('오민근의🪫모교 홍익대학교 전자전기공학부🔌')
 
-st.header('🤖 사이드바')
-st.sidebar.write('## 사이드바 텍스트')
-st.sidebar.checkbox('체크박스 1')
-st.sidebar.checkbox('체크박스 2')
-st.sidebar.radio('라디오 버튼', ['radio 1', 'radio 2', 'radio 3'])
-st.sidebar.selectbox('셀렉트박스', ['select 1', 'select 2', 'select 3'])
+st.header('🤖 컬럼 레이아웃')
+col_1, col_2, col_3 = st.columns([1,2,1]) # 컬럼 인스턴스 생성. 1:2:1 비율로 컬럼을 나눔
 
+with col_1:
+    st.write('## 1번 컬럼')
+    st.checkbox('이것은 1번 컬럼에 속한 체크박스 1')
+    st.checkbox('이것은 1번 컬럼에 속한 체크박스 2')
+
+with col_2:
+    st.write('## 2번 컬럼')
+    st.radio('2번 컬럼의 라디오 버튼', ['radio 1', 'radio 2', 'radio 3'])  # 동일한 라디오 버튼을 생성할 수 없음
+    # 사이드바에 이미 라디오 버튼이 생성되어 있기 때문에, 여기서는 라디오 버튼의 내용을 변경해야 오류가 발생하지 않음
+
+col_3.write('## 3번 컬럼')
+col_3.selectbox('3번 컬럼의 셀렉트박스', ['select 1', 'select 2', 'select 3'])
+# 사이드바에 이미 셀렉트박스가 생성되어 있기 때문에, 여기서는 셀렉트박스의 내용을 변경해야 오류가 발생하지 않음
 # 텍스트 출력
 st.write('# 1. 학과 소개')
 from PIL import Image
